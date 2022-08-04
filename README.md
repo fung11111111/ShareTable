cb85b10c4455bb7d2923162d459ac356a47d659d
fung11111111 <ifung9604@gamil.com>
2022-08-04 17:05:46 +0800

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# ShareTable
+
+# This project is going to help people find table and share food
---
+++ src/test/java/com/food/ShareTable/ShareTableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.food.ShareTable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareTableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
