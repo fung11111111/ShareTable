db0095929c19de839d16485250f046ac5e392f1c
fung11111111 <ifung9604@gamil.com>
2022-08-13 13:59:42 +0800

    [feat] add git log file path

--- src/main/java/com/food/ShareTable/restaurant/service/RestaurantService.java
+++ src/main/java/com/food/ShareTable/restaurant/service/RestaurantService.java
@@ -3,11 +3,13 @@ package com.food.ShareTable.restaurant.service;
 import com.food.ShareTable.restaurant.entity.Restaurant;
 import com.food.ShareTable.restaurant.exception.RestaurantExistsException;
 import com.food.ShareTable.restaurant.repository.RestaurantRepository;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.logging.Logger;
 
 @Service
 public class RestaurantService {
@@ -19,11 +21,12 @@ public class RestaurantService {
     }
 
 
+
     public List<Restaurant> getAllRestaurant() {
         return restaurantRepository.findAll();
     }
 
-    public Restaurant insertRestaurant(Restaurant restaurant) throws RestaurantExistsException{
+    public Restaurant insertRestaurant(Restaurant restaurant) throws RestaurantExistsException {
         List<Restaurant> existingRestaurants = restaurantRepository.findByNameAndAddress(restaurant.getName(), restaurant.getAddress());
         if (existingRestaurants != null && !existingRestaurants.isEmpty()) {
             throw new RestaurantExistsException();
